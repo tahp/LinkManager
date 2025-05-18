8f06367d0fccd761c1ee4dbd3c79f2c49a2437b2
tahp <djtahp@gmail.com>
2025-05-18 16:50:03 -0700

    jj

--- app.py
+++ app.py
@@ -292,5 +292,5 @@ def visit_link_action(link_id):
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
